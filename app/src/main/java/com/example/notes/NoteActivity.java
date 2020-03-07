16f0b711a196785025427ed71e1ce6486d4e4c06
bsebastian96 <bseb96@gmail.com>
2020-03-06 19:47:31 -0500

    Finalized app skeleton with basic functionality. Next step will be to implement the SQLite database.

--- app/src/main/java/com/example/notes/NoteActivity.java
+++ app/src/main/java/com/example/notes/NoteActivity.java
@@ -1,25 +1,46 @@
 package com.example.notes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.gesture.Gesture;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.example.notes.model.Note;
 
-public class NoteActivity extends AppCompatActivity {
+public class NoteActivity extends AppCompatActivity implements View.OnTouchListener,
+        GestureDetector.OnGestureListener,
+        GestureDetector.OnDoubleTapListener,
+        View.OnClickListener {
 
     private static final String TAG = "NoteActivity";
+    private static final int EDIT_MODE_ENABLED = 1;
+    public static final int EDIT_MODE_DISABLED = 0;
 
     // UI components
     private LinedEditText mLinedEditText;
     private EditText mEditTitle;
     private TextView mViewTitle;
+    private RelativeLayout mCheckContainer, mBackArrowContainer;
+    private ImageButton mCheck, mBackArrow;
 
     // Variables
     private boolean mIsNewNote;
+    private Note mInitialNote;
+    private GestureDetector mGestureDetector;
+    // Keeps track of the state
+    private int mMode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +49,240 @@ public class NoteActivity extends AppCompatActivity {
         mLinedEditText = findViewById(R.id.note_text);
         mEditTitle = findViewById(R.id.note_edit_title);
         mViewTitle = findViewById(R.id.note_text_title);
+        mCheckContainer = findViewById(R.id.check_container);
+        mBackArrowContainer = findViewById(R.id.back_arrow_container);
+        mCheck = findViewById(R.id.toolbar_check);
+        mBackArrow = findViewById(R.id.toolbar_back_arrow);
 
+        // Checks if incoming intent is for a new note or an already existing note
+        if (getIncomingIntent()) {
+            // this is a new note, (Edit mode)
+            setNewNoteProperties();
+            enableEditMode();
+        } else {
+            // this is NOT a new note (View mode)
+            setNoteProperties();
+            disableContentInteraction();
+        }
 
+        //
+        setListeners();
+    }
 
+    /*
+        If the incoming intent is sending a bundle with a Note, then that means it is an existing
+        note so we go to View mode.  If the incoming intent is not sending a Note, that means
+        the user clicks on creating a new Note, so we go right to Edit mode for the user to
+        create a new Note.
+     */
+    private boolean getIncomingIntent() {
+        if (getIntent().hasExtra("selected_note")) {
+            mInitialNote = getIntent().getParcelableExtra("selected_note");
+
+            mMode = EDIT_MODE_DISABLED;
+            mIsNewNote = false;
+            return false;
+        }
+        mMode = EDIT_MODE_ENABLED;
+        mIsNewNote = true;
+        return true;
+    }
+
+    // Sets properties for a new Note, not implemented yet
+    private void setNewNoteProperties() {
+        mViewTitle.setText("Note Title");
+        mEditTitle.setText("Note Title");
+    }
+
+    // Sets properties for viewing note that already existed
+    private void setNoteProperties() {
+        mViewTitle.setText(mInitialNote.getTitle());
+        mEditTitle.setText(mInitialNote.getTitle());
+        mLinedEditText.setText(mInitialNote.getContent());
+    }
+
+    private void enableEditMode() {
+        mBackArrowContainer.setVisibility(View.GONE);
+        mCheckContainer.setVisibility(View.VISIBLE);
+
+        mViewTitle.setVisibility(View.GONE);
+        mEditTitle.setVisibility(View.VISIBLE);
+
+        mMode = EDIT_MODE_ENABLED;
+
+        enableContentInteraction();
+    }
+
+    private void disableEditMode() {
+        mBackArrowContainer.setVisibility(View.VISIBLE);
+        mCheckContainer.setVisibility(View.GONE);
+
+        mViewTitle.setVisibility(View.VISIBLE);
+        mEditTitle.setVisibility(View.GONE);
+
+        mMode = EDIT_MODE_DISABLED;
+
+        disableContentInteraction();
+    }
+
+    /*
+        Unless LinedEditText is in edit mode, we want to disable interactions with it to prevent
+        single clicks opening edit mode for the content
+     */
+    private void disableContentInteraction() {
+        mLinedEditText.setKeyListener(null);
+        mLinedEditText.setFocusable(false);
+        mLinedEditText.setFocusableInTouchMode(false);
+        mLinedEditText.setCursorVisible(false);
+        mLinedEditText.clearFocus();
+    }
+
+    /*
+        If LinedEditText is in edit mode, we want interactions with it available.  We also have to
+        reassign the key listener to the default since the disable method sets it to null
+     */
+    private void enableContentInteraction() {
+        mLinedEditText.setKeyListener(new EditText(this).getKeyListener());
+        mLinedEditText.setFocusable(true);
+        mLinedEditText.setFocusableInTouchMode(true);
+        mLinedEditText.setCursorVisible(true);
+        mLinedEditText.requestFocus();
+    }
+
+    // Fixing keyboard not disappearing when leaving edit mode, android keyboard = soft keyboard
+    private void hideSoftKeyboard() {
+        InputMethodManager i = (InputMethodManager) this.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        View view = this.getCurrentFocus();
+        if (view == null) {
+            view = new View(this);
+        }
+
+        i.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+    /*
+        Initializes the touch listener and gesture detector. Once a touch is detected, onTouch() is
+        called, which passes the event to the gesture detector and runs a following method
+        corresponding to the type of touch that occurred
+     */
+    private void setListeners() {
+        mLinedEditText.setOnTouchListener(this);
+        mGestureDetector = new GestureDetector(this, this);
+        mViewTitle.setOnClickListener(this);
+        mCheck.setOnClickListener(this);
+        mBackArrow.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        return mGestureDetector.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        return false;
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent e) {
+        enableEditMode();
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+
+            case R.id.toolbar_check: {
+                hideSoftKeyboard();
+                disableEditMode();
+                break;
+            }
+
+            case R.id.note_text_title: {
+                enableEditMode();
+                // When clicking the title, we want the cursor focus to be at the end of the string
+                mEditTitle.requestFocus();
+                mEditTitle.setSelection(mEditTitle.length());
+                break;
+            }
+
+            case R.id.toolbar_back_arrow: {
+                // Destroys activity, can only be called in an activity, not a fragment
+                finish();
+                break;
+            }
+        }
+    }
+
+    /*
+        When we hit back when we are in Edit mode, we want to go back to view mode instead of
+        going back to the note list activity, so we have to intercept the back click by
+        overriding the following method and registering a click to the check mark which
+        goes back to view mode.
+     */
+    @Override
+    public void onBackPressed() {
+        if (mMode == EDIT_MODE_ENABLED) {
+            onClick(mCheck);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    /*
+        Called when activity is paused (onPause in lifecycle).
+        This is so we can save properties when activity is going to be destroyed.
+        Changing screen configuration causes the activity to be killed, then recreated so to
+        prevent changes in mode, we need to save the mode property.
+     */
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("mode", mMode);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        mMode = savedInstanceState.getInt("mode");
+        if (mMode == EDIT_MODE_ENABLED) {
+            enableEditMode();
+        }
     }
 }
--- app/src/main/java/com/example/notes/NotesListActivity.java
+++ app/src/main/java/com/example/notes/NotesListActivity.java
@@ -3,21 +3,26 @@ package com.example.notes;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.example.notes.adapters.NotesRecyclerAdapter;
 import com.example.notes.model.Note;
 import com.example.notes.util.VerticalSpacingItemDecorator;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
 // AppCompat refers to compatibility with more versions of Android
-public class NotesListActivity extends AppCompatActivity implements NotesRecyclerAdapter.OnNoteListener {
+public class NotesListActivity extends AppCompatActivity implements
+        NotesRecyclerAdapter.OnNoteListener,
+        View.OnClickListener {
 
     private static final String TAG = "NotesListActivity";
 
@@ -41,6 +46,9 @@ public class NotesListActivity extends AppCompatActivity implements NotesRecycle
 
         setSupportActionBar((Toolbar)findViewById(R.id.notes_toolbar));
         setTitle("Notes");
+
+        // Attaching onClick listener interface to floating action button
+        findViewById(R.id.fab).setOnClickListener(this);
     }
 
     private void insertFakeNotes() {
@@ -67,6 +75,8 @@ public class NotesListActivity extends AppCompatActivity implements NotesRecycle
         mNoteRecyclerAdapter = new NotesRecyclerAdapter(mNotes, this);
         mRecyclerView.setAdapter(mNoteRecyclerAdapter);
         mRecyclerView.addItemDecoration(itemDecorator);
+
+        new ItemTouchHelper(itemTouchHelperCallback).attachToRecyclerView(mRecyclerView);
     }
 
     @Override
@@ -77,4 +87,29 @@ public class NotesListActivity extends AppCompatActivity implements NotesRecycle
         intent.putExtra("selected_note", mNotes.get(position));
         startActivity(intent);
     }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, NoteActivity.class);
+        startActivity(intent);
+    }
+
+    // Implementing a way to delete an item entry by swiping it to the right or left
+    private void deleteNote(Note note) {
+        mNotes.remove(note);
+        mNoteRecyclerAdapter.notifyDataSetChanged();
+    }
+
+    // Designed to work with RecyclerViews.  SimpleCallback is a simpler version of Callback
+    private ItemTouchHelper.SimpleCallback itemTouchHelperCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT|ItemTouchHelper.LEFT) {
+        @Override
+        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+            return false;
+        }
+
+        @Override
+        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+            deleteNote(mNotes.get(viewHolder.getAdapterPosition()));
+        }
+    };
 }
